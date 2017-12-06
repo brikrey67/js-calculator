57c45096b7bc377dc272d9fedb34fa72b33aa3ff
Brian Reynolds <brikrey@gmail.com>
2017-12-06 10:04:29 -0500

    setup calc object boilerplate

--- calculator.js
+++ calculator.js
@@ -1,2 +1,22 @@
 var calculator = {
+
+
+    addition: function(){
+        console.log("addition")
+      },
+    subtraction: function() {
+        console.log("subtract")
+    },
+    multiply: function() {
+        console.log("multiple")
+    },    
+    divide: function() {
+        console.log("divide")
+    },
+    exponent: function() {
+        console.log("exponent")
+    },
+    factorial: function() {
+        console.log("factorial")
+    }
 }
